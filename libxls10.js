7ab397ac568cd64a0941541289034979e623aec1
Richard Holland <richard.holland@starstone.co.nz>
2020-04-08 17:22:20 +1200

    clean up, alpha, formal test suite to follow

--- libxls10.js
+++ libxls10.js
@@ -1,3 +1,7 @@
+// XLS-10 Token Manipulation Library
+// Version: 0.8
+// Author: Richard Holland @codetsunami
+
 //todo: verify utf-8 and ascii work correctly
 
 const assert = require('assert').strict;
@@ -26,20 +30,45 @@ function replaceAt(s, index, char) {
     return s.substr(0,index) + char + s.substr(index+1)
 }
 
+
 class XLS10Token {
     #schema = {}
+
+    // schema must be a JSON format describing field types in order, see README.md
+    // token is optional and may be a 40 nibble hexidecimal string representing an existing token to pre-load into this object
     constructor(schema, token = null) {
+        var rawschema = schema
         schema = JSON.parse(schema)
-        var count = 0
+
+        // JSON isn't required according to RFC spec to maintain key position, however in our use-case key position is extremely important.
+        // Therefore we will enforce it via manual key parsing. We'll also take this opportunity to clean up the provided schema, in sequence, for storage in the _schema member
+        var key_position =  {}
+        var key_regex = /^\s*"([^"]+)" *:/img;
+        var match = key_regex.exec(rawschema);
+        var position = 0
+        var cleanschema = "{"
+        while (match != null) {
+            // matched text: match[0]
+            // match start: match.index
+            // capturing group n: match[n]
+            key_position[match[1]] = position++
+            cleanschema += '"' + match[1] + '":"' + schema[match[1]] + '"'
+            match = key_regex.exec(rawschema);
+            cleanschema += (match ? ',' : '')
+        }
+        cleanschema += "}"
+
+
+        // now process the provided datatypes
         for (var key in schema) {
-            var dtype = schema[key]
-            // technically they shouldn't be allowed to mix case but we're all about compatibility
-            dtype = dtype.toLowerCase()
-            // preflight check
+            var dtype = schema[key].toLowerCase()
+
+            // parse the datatype entry
             var pieces = dtype.match(/((u)?(int|char)([0-9]+)_t|bit|bool)(?:\[([0-9]+)\])?/)
 
-            assert(pieces, "data type should match specification, see readme.md")
+            assert(pieces, "data type should match specification, see README.md")
 
+            // handle special case when bit or bool is specified, these are really uint1_t's
             if (pieces[1] == 'bit' || pieces[1] == 'bool') {
                 pieces[2] = 'u'
                 pieces[3] = 'int'
@@ -47,6 +76,8 @@ class XLS10Token {
                 pieces[5] = '1'
             }
 
+            // the datatype is decomposed into four pieces of information used internally
+            // note that all datatypes are considered to be arrays of length 1 or more
             var entry = { 
                 signed: pieces[2] != 'u',
                 ischar: pieces[3] == 'char',
@@ -54,66 +85,88 @@ class XLS10Token {
                 arraylen: pieces[5] != null ? parseInt(pieces[5]) : 1
             }
 
-            // prefill with valid value state ( 0's )
-            entry['value'] = ( entry['ischar'] ? 0n : Array(entry['arraylen']).fill(0n) )
-
-
-            // filter out cowboy operators
+            // check for invalid combinations
             assert(! (entry['signed'] && entry['ischar']), 'character types must be unsigned')
             assert(entry['bitlen'] > 0 && entry['bitlen'] <= 152, 'type bit length must be a positive integer less than 153')
             assert(entry['arraylen'] > 0 , 'type array length must be a positive integer')
             assert(! (entry['signed'] && entry['bitlen'] == 1), 'single bits cannot be signed')
-
             assert(!entry['ischar'] || (entry['bitlen'] >= 6 && entry['bitlen'] <= 8), "character type specified but unknown encoding, please use int instead for custom encoding. valid encodings are six-bit: uchar6_t, ascii: uchar7_t, utf-8: uchar8_t")
 
-            entry['position'] = count++
+            // record the true position of the field according to the originally parsed schema
+            entry['position'] = key_position[key]
             
-            // execution to here means it's probably valid datatype, add it
+            // execution to here means it's a valid datatype, add it assuming it's not somehow a duplicate key
             assert(!(key in this.#schema), "duplicate key " + key)
             this.#schema[key] = entry
         }
 
-    
+        // ensure mandatory fields have been included in the schema
         assert('type' in this.#schema && !this.#schema['type']['signed'] && !this.#schema['type']['ischar'] && this.#schema['type']['bitlen'] == 8 && this.#schema['type']['arraylen'] == 1, "mandatory uint8_t `type` field not specified")
         assert('subtype' in this.#schema && !this.#schema['subtype']['signed'] && !this.#schema['subtype']['ischar'] && this.#schema['subtype']['bitlen'] == 16 && this.#schema['type']['arraylen'] == 1, "mandatory uint16_t `subtype` field not specified")
-
 
         // check the bit count sums to less than 161
         var totalbits = 0
         for (var key in schema) 
             totalbits += this.#schema[key]['bitlen'] * this.#schema[key]['arraylen']
-        
-        entry['value'] = 0
-
         assert(totalbits > 0 && totalbits <= 160, "total bit count of schema is too large, should be 160 bits or fewer, currently " + totalbits)
 
+        // establish getters and setters for the object. the end developer is also free to use get(field) and set(field,value) if they wish
+        for (var key in this.#schema)
+            ((obj, key)=>{
+                Object.defineProperty(this, key, {
+                    enumerable: true,
+                    get: ()=>{return obj.get(key)},
+                    set: (newval)=>{return obj.set(key,newval)}
+                })
+            })(this, key)
+
+        // set up a read only _schema member containing the cleaned up original schema provided by the developer
+        Object.defineProperty(this, '_schema', {
+            enumerable: true,
+            value: cleanschema,
+            writable: false
+        })
+
+        // set the initial state of the token to 0's
+        this.reset()
+
+        // if a hex token was provided during construction set it up now
         if (token)
             this.parse(token)
     }
 
+    // returns the current value in the token object associated with a particular field, in decoded form.
     get(field) {
+        // ensure the field exists
         assert(field in this.#schema, "field " + field + " not found in specified xls10 schema")
-        var entry = this.#schema[field]
-        // conversion to js datatype required
 
+        var entry = this.#schema[field]
         var value = entry['value']
 
-        // is it a char?
+        // is it a string of some sort?
         if (entry['ischar']) {
+
+            // only three string encodings are supported, this should never fire because it is enforced in constructor
             assert(entry['bitlen'] >= 6 && entry['bitlen'] <= 8, "three acceptable character encodings exist: six-bit, 7bit ascii, and utf-8")
 
-            var enc = entry['bitlen']
-            var len = entry['arraylen']
+            // check the internal representation of the string is correct
+            assert(typeof(value) == 'bigint', 'internal representation of character array state for field ' + field + ' is corrupt, expecting bigint, found ' + typeof(value))
 
-            // bitmask
-            var mask = BigInt((1 << enc) - 1)
+            // enc is the number of bits per character
+            var enc = BigInt(entry['bitlen'])
+            // len is the number of characters, all strings are fixed length records
+            var len = BigInt(entry['arraylen'])
+
+            // bitwise and mask for the size of one character
+            var mask = (1n << enc) - 1n
 
             // arraylen tells us how many characters we are working with
             var s = ""
             var nextupper = false
 
-            for (var i = len - 1; i >= 0; --i) {
-                var shift = BigInt(i * enc)
+            // todo: consider optimising this loop, probably only need one shift operation per loop
+            for (var i = len - 1n; i >= 0n; --i) {
+                var shift = i * enc
                 var character = ( mask << shift ) & value
                 character >>= shift
 
@@ -121,7 +174,6 @@ class XLS10Token {
                     nextupper = true
                     continue
                 }
-
 
                 var c = ( enc == 6 ? sixbit[parseInt(character)] : String.fromCharCode(parseInt(character)) )
                 s += ( nextupper ? c.toUpperCase() : c )
@@ -132,11 +184,14 @@ class XLS10Token {
  
         } else {
             // datatype is integer
-
+            if (entry['arraylen'] == 1) 
+                return value[0]
+            // datatype is an array
             return value
         }
     }
 
+    // set the value of a field in the token object, in decoded form
     set(field, value, allowtruncation) {
 
         assert(field in this.#schema, "field `" + field + "` not found in specified xls10 schema")
@@ -189,7 +244,13 @@ class XLS10Token {
         } else {
             // datatype is integer
             var field_signed = entry['signed']
-            if (typeof(value) == 'number' || typeof(value) == 'bigint') value = [value]
+
+            if (typeof(value) != 'object') value = [value]
+
+
+            if (entry['bitlen'] == 1)
+                for (var x in value)
+                    if (typeof(value[x]) == 'boolean') value[x] = ( value[x] ? 1n : 0n ) 
 
             assert(typeof(value) == 'object' && 'length' in value && value.length == len, "must provide a value or array of values of the correct length corresponding to the datatype")
 
@@ -219,20 +280,23 @@ class XLS10Token {
     // reset all values in the object, preserving the (immutable) schema
     reset() {
         for (var field in this.#schema) {
+            if (field == '_schema') continue
             var entry = this.#schema[field]
             this.#schema[field]['value'] = ( entry['ischar'] ? 0n : Array(entry['arraylen']).fill(0n) )
         }
     }
 
 
+    // parse a token from hexadecimal to usable object
     parse(token) {
         
+        // enforce hex encoding
         assert(typeof(token) == 'string' && token.length == 40 && token.match(/^[a-fA-F0-9]{40}$/), "must provide a 40 nibble hexidecimal string representing the token")
 
         // clear out anything currently residing in the object's value fields
         this.reset()
 
-        // convert to big int to get started 
+        // convert to big int to get started -- this is big endian, so is bigint.toString(16) so we don't need to worry about endianness in encoding/decoding to bigint
         token = BigInt('0x' + token) 
 
         // ensure we have the canonical field order
@@ -245,18 +309,19 @@ class XLS10Token {
 
         assert(Object.keys(keys).length == Object.keys(this.#schema).length, "schema has duplicate position entry")
 
-        // remove right hand padding
+        // remove right hand padding (this is when a token's schema is smaller than 160 bits, it has right padded 0's)
         token >>= (160n - schemabitlen)
 
         // parse right to left, shifting as we go
         for (var fieldid = Object.keys(keys).length-1; fieldid >= 0; --fieldid) {
+
+            // pull out values to make code easier to read below
             var field = keys[fieldid]
             var entry = this.#schema[field]
             var enc = BigInt(entry['bitlen'])
             var len = BigInt(entry['arraylen'])
             var value = entry['value']
             var signed = entry['signed']
-
 
             if (entry['ischar']) {
                 // simply import the bits directly without modification
@@ -265,23 +330,28 @@ class XLS10Token {
                 token >>= (enc * len)
             } else {
                 // it's a number or array of numbers
-
+                
+                // this is the bit-mask for a single entry in the array
                 var bitmask = (1n << enc) - 1n
-
+                
+                // this is the bit-mask for the most significant bit aka sign bit, we can use this to determine if it's a negative
                 var msbmask = (1n << (enc - 1n))
 
                 for (var n = 0; n < len; ++n) {
+                    // mask off the current integer
                     var value = token & bitmask
 
-                    // handle 2's complement
+                    // handle 2's complement if the type is signed and we have a sign bit
                     if (signed && (token & msbmask)) {
                         value -= msbmask
                         value++
                         value *= -1n
                     }
                     
+                    // set the decoded value in the object's representation
                     this.#schema[field]['value'][n] = value
 
+                    // shift to the next integer
                     token >>= enc
                 }
             }
@@ -289,6 +359,8 @@ class XLS10Token {
     }
 
 
+
+    // convert object's internal representation into a 40 nibble hexadecimal token representation according to the schema    
     tokenize() {
        
         // the schema keys should already be in the correct order but we're going to double check that
@@ -301,6 +373,7 @@ class XLS10Token {
 
         assert(Object.keys(keys).length == Object.keys(this.#schema).length, "schema has duplicate position entry")
 
+        // since the largest token is 160 bits and 2^160-1 will easily fit inside a bigint, we will use bigint until the very end
         var output = 0n
 
         var fieldcount = Object.keys(keys).length
@@ -308,18 +381,25 @@ class XLS10Token {
             var field = keys[fieldid]
             var entry = this.#schema[field]
 
+            // pull these values out for readability of code below
             var enc = BigInt(entry['bitlen'])
             var len = BigInt(entry['arraylen'])
             var value = entry['value']
             var signed = entry['signed']
 
+            // is it a string type?
             if (entry['ischar']) {
+                // string types are encoded and decoded in the setter and getter, so they are ready to be put into the token from raw internal state
                 assert(typeof(value) == 'bigint', "attempted to tokenize character type that isn't stored as a bigint")
                 output <<= (enc * len)
-                output += BigInt(value)
+                output += value
             } else {
+                // integer types (including integer arrays) are stored as signed bigints internally, so should be converted to match encoding here
+
+                // all int types are internally recorded as an array of bigints of length 1 or greater
                 assert(typeof(value) == 'object' && 'length' in value, "attempted to tokenize integer type that isn't stored as an array of numbers")
 
+                // process the array of ints
                 for (var n = value.length - 1; n >= 0; --n) {
                     var v = value[n]
                     assert(typeof(v) == 'bigint', "attempted to tokenize non-number as integer type")
@@ -331,8 +411,7 @@ class XLS10Token {
                         v += (1n << (enc - 1n))
                     }
 
-//                    console.log("outputing: " + value[n] + " as " + v + " | 0x" + v.toString(16) + " | " + v.toString(2) )
-
+                    // make space in the output token for the int then add it
                     output <<= enc
                     output += v
                 }
@@ -344,16 +423,17 @@ class XLS10Token {
         // right pad with 0's to make up any missing bits from 160 bit total
         output <<= (160n - schemabitlen)
 
-
         // convert to hex
         var outhex = output.toString(16)
         assert(outhex.length <= 40, "output hex too long (" + outhex.length + " nibbles), schema must be corrupted")
+
+        // left pad any missing 0's from the hex (should only ever be at most 1, due to right padding above)
         outhex = "0".repeat(40 - outhex.length) + outhex
 
         return outhex
-
     }
 
+    // a human readable current value representation of the token, designed for printing, not reconstruction
     toString() {
         var keys = {}
         for (var field in this.#schema)
